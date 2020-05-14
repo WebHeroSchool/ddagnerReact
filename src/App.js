e1f80a8f8540ed89b1b67d6ec1d49ac1c144c7b8
Даниил Фефелов <feden2069@gmail.com>
2020-05-14 19:18:56 +0500

    lesson15

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const isLogo = true;
-const numberOfProject = 1;
-const desireToLearn = true;
+import * as nums from './numbers.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1 style={
-          { color: '#61dafb',
-            margin: 0
-          }
-        }>
-          Hi, It's my the {numberOfProject} project!
-        </h1>
-        <p>
-          I did {numberOfProject - 1} react project before 
-        </p>
-        <p>
-          {isLogo && 'It has a mesmerizing logo'}
-        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -33,14 +17,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {desireToLearn ? 'Learn React' : "Don't use the link"}
+          Learn React
+          <p>count * length = {nums.count*nums.length}</p>
         </a>
-        <p>
-          undefined: {undefined},
-          null: {null},
-          false: {false},
-          true: {true}
-        </p>
       </header>
     </div>
   );
---
+++ src/numbers.js
@@ -0,0 +1,3 @@
+const count = 74;
+const length = 9;
+export { count, length };
